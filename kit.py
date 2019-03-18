e54e4261a1548e6d79b9e127a20efc21419eb5d7
Andrew Booker <improvizone@gmail.com>
2019-03-18 23:35:01 +0000

    correcting pad param tag name, plus adding commented placeholders for other pad settings

--- kit.py
+++ kit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+from datetime import date
 import xml.dom.minidom
+
 doc = xml.dom.minidom.parseString("<KitPrm/>")
 
 def kitNode(onto, name):
@@ -18,17 +20,32 @@ kitPrm = doc.documentElement
 kitParam(kitPrm, "Level", 100)
 kitParam(kitPrm, "Tempo", 790)
 
-setName(kitPrm, "20190318")
+setName(kitPrm, date.today().strftime("%Y%m%d"))
 for i in range(16):
 	kitParam(kitPrm, "SubNm%d" % i, 0)
 	
 
 for i in range(15):
-	pad = kitNode(kitPrm, "PadParam")
+	pad = kitNode(kitPrm, "PadPrm")
 	kitParam(pad, "Wv", -1)
-	kitParam(pad, "SubWv", -1)
+	#kitParam(pad, "WvLevel", 100)
+	#kitParam(pad, "WvPan", 15)
+	#kitParam(pad, "PlayMode", 0)
+	#kitParam(pad, "OutAsgn", 0)
+	#kitParam(pad, "MuteGrp", 0)
+	#kitParam(pad, "TempoSync", 0)
 	kitParam(pad, "PadMidiCh", 15)
 	kitParam(pad, "NoteNum", i + 1)
+	#kitParam(pad, "MidiCtrl", 0)
+	#kitParam(pad, "Loop", 0)
+	#kitParam(pad, "TrigType", 0)
+	#kitParam(pad, "GateTime", -1)
+	#kitParam(pad, "Dynamics", 1)
+	#kitParam(pad, "VoiceAsgn", 1)
+	#kitParam(pad, "Reverse", 0)
+	kitParam(pad, "SubWv", -1)
+	#kitParam(pad, "SubWvLevel", 100)
+	#kitParam(pad, "SubWvPan", 15)
 
 kitPrm.writexml(open("D:\\gear\\spd-sx\\sandbox\\Roland\\SPD-SX\\KIT\\kit099.spd", "w"), addindent="\t", newl="\n")
 kitPrm.writexml(open("E:\\Roland\\SPD-SX\\KIT\\kit099.spd", "w"), addindent="\t", newl="\n")
