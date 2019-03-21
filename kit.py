c1c2ffb8687afaca75107d59b94f5ecbf5d4c1c6
Andrew Booker <improvizone@gmail.com>
2019-03-21 23:16:48 +0000

    starting to create kit patches for new TBB songs

--- kit.py
+++ kit.py
@@ -3,49 +3,72 @@
 from datetime import date
 import xml.dom.minidom
 
-doc = xml.dom.minidom.parseString("<KitPrm/>")
-
-def kitNode(onto, name):
+def kitNode(doc, onto, name):
 	return onto.appendChild(doc.createElement(name))
 
-def kitParam(onto, name, value):
-	kitNode(onto, name).appendChild(doc.createTextNode(str(value)))
+def kitParam(doc, onto, name, value):
+	kitNode(doc, onto, name).appendChild(doc.createTextNode(str(value)))
 	
-def setName(onto, name):
+def setName(doc, onto, name):
 	for i in range(8):
-		kitParam(onto, "Nm%d" % i, ord(name[i]))
-	
-	
-kitPrm = doc.documentElement
-kitParam(kitPrm, "Level", 100)
-kitParam(kitPrm, "Tempo", 790)
+		kitParam(doc, onto, "Nm%d" % i, ord(name[i]))
 
-setName(kitPrm, date.today().strftime("%Y%m%d"))
-for i in range(16):
-	kitParam(kitPrm, "SubNm%d" % i, 0)
+
+def specItemFrom(padSpecs, item, i):
+	k = str(i + 1)
+	if (k in padSpecs):
+		spec = padSpecs[k]
+		if (item in spec):
+			return spec[item]
+	return -1
+		
+
+def createKit(zIndex, name, tempo, padSpecs):
+	doc = xml.dom.minidom.parseString("<KitPrm/>")
+	kitPrm = doc.documentElement
+	kitParam(doc, kitPrm, "Level", 100)
+	kitParam(doc, kitPrm, "Tempo", tempo * 10)
+	setName(doc, kitPrm, name)
 	
+	for i in range(16):
+		kitParam(doc, kitPrm, "SubNm%d" % i, 0)
+	
+	for i in range(15):
+		pad = kitNode(doc, kitPrm, "PadPrm")
+		kitParam(doc, pad, "Wv", specItemFrom(padSpecs, "sound", i))
+		kitParam(doc, pad, "WvLevel", 100)
+		kitParam(doc, pad, "WvPan", 15)
+		kitParam(doc, pad, "PlayMode", 0)
+		kitParam(doc, pad, "OutAsgn", 0)
+		kitParam(doc, pad, "MuteGrp", 0)
+		kitParam(doc, pad, "TempoSync", 0)
+		kitParam(doc, pad, "PadMidiCh", 15)
+		kitParam(doc, pad, "NoteNum", specItemFrom(padSpecs, "note", i))
+		kitParam(doc, pad, "MidiCtrl", 0)
+		kitParam(doc, pad, "Loop", 0)
+		kitParam(doc, pad, "TrigType", 0)
+		kitParam(doc, pad, "GateTime", -1)
+		kitParam(doc, pad, "Dynamics", 1)
+		kitParam(doc, pad, "VoiceAsgn", 1)
+		kitParam(doc, pad, "Reverse", 0)
+		kitParam(doc, pad, "SubWv", -1)
+		kitParam(doc, pad, "SubWvLevel", 100)
+		kitParam(doc, pad, "SubWvPan", 15)
 
-for i in range(15):
-	pad = kitNode(kitPrm, "PadPrm")
-	kitParam(pad, "Wv", -1)
-	#kitParam(pad, "WvLevel", 100)
-	#kitParam(pad, "WvPan", 15)
-	#kitParam(pad, "PlayMode", 0)
-	#kitParam(pad, "OutAsgn", 0)
-	#kitParam(pad, "MuteGrp", 0)
-	#kitParam(pad, "TempoSync", 0)
-	kitParam(pad, "PadMidiCh", 15)
-	kitParam(pad, "NoteNum", i + 1)
-	#kitParam(pad, "MidiCtrl", 0)
-	#kitParam(pad, "Loop", 0)
-	#kitParam(pad, "TrigType", 0)
-	#kitParam(pad, "GateTime", -1)
-	#kitParam(pad, "Dynamics", 1)
-	#kitParam(pad, "VoiceAsgn", 1)
-	#kitParam(pad, "Reverse", 0)
-	kitParam(pad, "SubWv", -1)
-	#kitParam(pad, "SubWvLevel", 100)
-	#kitParam(pad, "SubWvPan", 15)
+	kitFn = "kit%03d.spd" % zIndex
+	kitPrm.writexml(open("D:\\gear\\spd-sx\\sandbox\\Roland\\SPD-SX\\KIT\\%s" % kitFn, "w"), addindent="\t", newl="\n")
+	#kitPrm.writexml(open("E:\\Roland\\SPD-SX\\KIT\\%s" % kitFn, "w"), addindent="\t", newl="\n")
+	print("kit %s created in %s" % (name, kitFn))
 
-kitPrm.writexml(open("D:\\gear\\spd-sx\\sandbox\\Roland\\SPD-SX\\KIT\\kit099.spd", "w"), addindent="\t", newl="\n")
-kitPrm.writexml(open("E:\\Roland\\SPD-SX\\KIT\\kit099.spd", "w"), addindent="\t", newl="\n")
+#createKit(98, date.today().strftime("%Y%m%d"), 83, {}) 
+
+kn = 50
+set = __import__("tbb2019")
+for n, cl in set.__dict__.items():
+	if isinstance(cl, type):
+		createKit(kn, n[:8], cl.tempo, cl.pads)
+		kn += 1
+
+
+
+
---
+++ tbb2019.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+class ItCouldBeHome():
+	tempo = 90
+	pads = {"4":{"sound":0,"note":64},"7":{"sound":40}}
+
+
+class BraveDreams():
+	tempo = 93
+	pads = {}
+
+
+class TheWarOnMe():
+	tempo = 120
+	pads = {}
