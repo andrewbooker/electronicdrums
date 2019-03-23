6dd8dc9bbb4870c53512fcb81b7a65a4d2d527a1
Andrew Booker <improvizone@gmail.com>
2019-03-23 00:15:16 +0000

    building a dictionary of sounds and starting to reference them by name in kit setup

--- tbb2019.py
+++ tbb2019.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+from sounds import sounds
+
 
 class ItCouldBeHome():
 	tempo = 90
-	pads = {"4":{"sound":0,"note":64},"7":{"sound":40}}
+	pads = {"1":{"sound":sounds["Cym_Fx1"]},
+			"4":{"sound":sounds["Clap_808"],"note":64},
+            "7":{"sound":40}}
 
 
 class BraveDreams():
