4ca7672014efe8c5cf7b688f04be3b4bd4e892ef
Andrew Booker <improvizone@gmail.com>
2019-06-29 07:41:50 +0000

    more selction of sounds for the entire iZ kit

--- iZgenKits.py
+++ iZgenKits.py
@@ -74,10 +74,54 @@ class KitSounds():
 				 "Kick_Proc3", #56 #00/Kick__17.wav
 				 "Kick_Proc4", #57 #00/Kick__18.wav
 				 "Kick_Proc5"] #58 #00/Kick__19.wav
-				 
-	perc = [] #left foot, bottom left SPD-SX, pad rim
-	backbeat = [] #pad head
-	cym = [] #top left SPD-SX
+	
+	leftFoot = ["Tom_808_H",  #:128 #01/Tom_8.wav
+				"Tom_808_L",  #:129 #01/Tom_8_01.wav
+				"Tom_808_M",  #:130 #01/Tom_8_02.wav
+				"Tom_Acou_H", #:131 #01/Tom_A.wav
+				"Tom_Acou_L", #:132 #01/Tom_A_01.wav
+				"Tom_Acou_M", #:133 #01/Tom_A_02.wav
+				"Tom_Roto_H", #:137 #01/Tom_R.wav
+				"Tom_Roto_L", #:138 #01/Tom_R_01.wav
+				"Tom_Roto_M"] #:139 #01/Tom_R_02.wav
+	
+	backbeat = ["Snare_110", #103 #01/Snare.wav
+				"Snare_808", #104 #01/Snare_01.wav
+				"Snare_909", #105 #01/Snare_02.wav
+				"Snare_Acou1", #106 #01/Snare_03.wav
+				"Snare_Acou2", #107 #01/Snare_04.wav
+				"Snare_DbS", #108 #01/Snare_05.wav
+				"Snare_DnB1", #109 #01/Snare_06.wav
+				"Snare_DnB2", #110 #01/Snare_07.wav
+				"Snare_Edrs", #111 #01/Snare_08.wav
+				"Snare_Hph", #112 #01/Snare_09.wav
+				"Snare_Hse1", #113 #01/Snare_10.wav
+				"Snare_Hse2", #114 #01/Snare_11.wav
+				"Snare_Proc1", #115 #01/Snare_12.wav
+				"Snare_Proc2", #116 #01/Snare_13.wav
+				"Snare_Proc3", #117 #01/Snare_14.wav
+				"Snare_Proc4", #118 #01/Snare_15.wav
+				"Snare_Proc5", #119 #01/Snare_16.wav
+				"Snare_Proc6", #120 #01/Snare_17.wav
+				"SnareXs_1", #121 #01/Snare_18.wav
+				"SnareXs_2", #122 #01/Snare_19.wav
+				"SnareXs_3", #123 #01/Snare_20.wav
+				"SnareXs_4", #124 #01/Snare_21.wav
+				"SnareXs_5", #125 #01/Snare_22.wav
+				"SnareXs_6", #126 #01/Snare_23.wav
+				"SnareXs_7"] #:127 #01/Snare_24.wav]
+	
+	perc = ["Clap_Hse2", #:4 #00/Clap__04.wav
+			"Clap_Min",  #:5 #00/Clap__05.wav
+			"Clap_Verb1", #:6 #00/Clap__06.wav
+			"P_GanzaTap",
+			"SE_VerbPf"]
+	
+	cym = ["Cym_808",
+		   "Ride_Acou",  #:82 #00/Ride_.wav
+		   "Ride_DnB",   #:83 #00/Ride__01.wav
+	       "Ride_Proc1", #:84 #00/Ride__02.wav
+		   "Ride_Proc1Bl"]#:85 #00/Ride__03.wav
 
 def generate(r, m, t):
 	notes = Notes(7, roots[r], modes[m])
@@ -90,15 +134,15 @@ def generate(r, m, t):
 	k.pads.append({"sound": wav["P_Triangl_op"], "channel": 0, "note": notes.note(4)})
 	k.pads.append({"sound": wav["P_Triangl_op"], "channel": 0, "note": notes.note(5)})
 	k.pads.append({"sound": wav["P_Triangl_op"], "channel": 0, "note": notes.note(6)})
-	k.pads.append({"sound": wav["Cym_808"]})
+	k.pads.append({"sound": wav[KitSounds.any(KitSounds.cym)]})
 	k.pads.append({"sound": wav["P_Triangl_op"], "channel": 0, "note": notes.note(2)})
 	k.pads.append({"sound": wav["P_Triangl_op"], "channel": 0, "note": notes.note(3)})
-	k.pads.append({"sound": wav["P_GanzaTap"]})
+	k.pads.append({"sound": wav[KitSounds.any(KitSounds.perc)]})
 	k.pads.append({"sound": wav["P_Triangl_op"], "channel": 0, "note": notes.note(0)})
 	k.pads.append({"sound": wav["P_Triangl_op"], "channel": 0, "note": notes.note(1)})
 	k.pads.append({"sound": wav[KitSounds.any(KitSounds.rightFoot)]})
-	k.pads.append({"sound": wav["SnareXs_4"]})
-	k.pads.append({"sound": wav["SnareXs_7"]})
-	k.pads.append({"sound": wav["SE_VerbPf"]})	
+	k.pads.append({"sound": wav[KitSounds.any(KitSounds.leftFoot + KitSounds.backbeat)]})
+	k.pads.append({"sound": wav[KitSounds.any(KitSounds.backbeat)]})
+	k.pads.append({"sound": wav[KitSounds.any(KitSounds.perc)]})
 
 	return k
