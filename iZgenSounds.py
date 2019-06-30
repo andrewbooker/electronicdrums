0a896d3b7cbc59314e65409a67b1af894afb6d34
Andrew Booker <improvizone@gmail.com>
2019-06-30 10:57:08 +0000

    stretching one of the component waves

--- iZgenSounds.py
+++ iZgenSounds.py
@@ -2,7 +2,24 @@
 
 import soundfile as sf
 import os
+import math
 
+
+class Stretch():
+	def __init__(self):
+		self.buffer = []
+		
+	def on(self, d, i, f):
+		self.buffer.append(d)
+		if (len(self.buffer) == 1):
+			return d
+		
+		p = i / f
+		u = math.floor(p)
+		l = math.ceil(p)
+		dp = p - l
+		return ((1 - dp) * self.buffer[l]) + (dp * self.buffer[u])
+		
 class Avg():
 	def on(self, d1, d2, i, size):
 		return (d1 * 0.5) + (d2 * 0.5)
@@ -58,13 +75,14 @@ def combine(fnOnto, s1, s2, op):
 	done = False
 
 	size = max(f1.frames, f2.frames)
+	stretch = Stretch()
 	while (not done):
 		hasF1 = f1.tell() < f1.frames
 		hasF2 = f2.tell() < f2.frames
 		
 		data = [0, 0]
 		if hasF1:
-			data[0] = f1.read(1)[0]
+			data[0] = stretch.on(f1.read(1)[0], i, 1.6)
 		if hasF2:
 			data[1] = f2.read(1)[0]
 			
