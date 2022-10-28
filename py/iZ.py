9a0d827669f10ae1118e35f1e4b363843c8f3531
Andrew Booker <improvizone@gmail.com>
2022-10-28 21:09:42 +0100

    getting kit generation working on linux

--- py/iZ.py
+++ py/iZ.py
@@ -7,26 +7,33 @@ from iZgenKits import generate, modeNames
 from sysConfig import SystemConfig
 from effects import *
 from random import randint
+import json
+import os
 
+def config():
+    with open("config.json") as conf:
+        return json.load(conf)
+
+loc = os.path.join(config()["mediaLoc"], "Roland", "SPD-SX")
 master = 0
 sub = 1
 
 if (len(sys.argv) < 2):
-	print("iZ <key> [<tempo>] [<apply-master-fx-to>] [<delay-subdivision>] [<allow-fx-mod>]")
-	print("eg iZ.py F# 83 kit")
-	print("or iZ.py C 110 korg intra")
-	print("or iZ.py Bb 150 korg poly 1")
-	print("or iZ.py A")
-	exit()
+    print("iZ <key> [<tempo>] [<apply-master-fx-to>] [<delay-subdivision>] [<allow-fx-mod>]")
+    print("eg iZ.py F# 83 kit")
+    print("or iZ.py C 110 korg intra")
+    print("or iZ.py Bb 150 korg poly 1")
+    print("or iZ.py A")
+    exit()
 
 class FxKorg():
-	korgAssign = master
-	allowedFx = [Slicer, TapeEcho, TouchWah]
-		  
+    korgAssign = master
+    allowedFx = [Slicer, TapeEcho, TouchWah]
+
 class FxKit():
-	korgAssign = sub
-	allowedFx = [RingMod, Phaser, FilterPlusDrive, Distortion, TouchWah, PitchShift, Vibrato, Reverb, Slicer]
-	
+    korgAssign = sub
+    allowedFx = [RingMod, Phaser, FilterPlusDrive, Distortion, TouchWah, PitchShift, Vibrato, Reverb, Slicer]
+
 effModes = {"kit": FxKit, "korg": FxKorg}
 
 key = sys.argv[1].upper()
@@ -48,50 +55,50 @@ c.inAssign = mode.korgAssign
 c.fxModOn = allowFxMod
 c.masterFx = masterFx.createRandom()
 
-c.createIn("E:\\Roland\\SPD-SX\\SYSTEM\\sysparam.spd")
+c.createIn(os.path.join(loc, "SYSTEM", "sysparam.spd"))
 
 kitFx1 = Thru
 kitFx2 = any(FxKit.allowedFx, [masterFx])
 if (c.fx1On() == 1):
-	kitFx1 = any(FxKit.allowedFx, [masterFx, kitFx2])
-	print("FX1 %s" % kitFx1.__name__)
+    kitFx1 = any(FxKit.allowedFx, [masterFx, kitFx2])
+    print("FX1 %s" % kitFx1.__name__)
 
 print("FX2 %s" % kitFx2.__name__)
 
 idx = 70
 for modeName in modeNames:
 
-	kitDef = generate(key, modeName, tempo)
-	kitDef.pan = 0
+    kitDef = generate(key, modeName, tempo)
+    kitDef.pan = 0
 
-	kitDef.fx1 = kitFx1.createRandom() if c.fx1On() == 1 else kitFx1()
-	kitDef.fx2 = kitFx2.createRandom()
-		
-	# assign all sounds using c.kitAssign() and c.fx1On()
-	padOutMaster = 0
-	padOutFx1 = 1
-	padOutFx2 = 2
-	padOutSub = 3
+    kitDef.fx1 = kitFx1.createRandom() if c.fx1On() == 1 else kitFx1()
+    kitDef.fx2 = kitFx2.createRandom()
 
-	topKitOut = padOutFx1 if (c.fx1On() == 1) else padOutFx2
-	korgPercOut = padOutMaster if (c.inAssign == sub) else topKitOut
-	midKitOut = padOutFx2
-	bdOut = padOutSub # allow padOutFx2 if not RingMod
+    # assign all sounds using c.kitAssign() and c.fx1On()
+    padOutMaster = 0
+    padOutFx1 = 1
+    padOutFx2 = 2
+    padOutSub = 3
 
-	kitDef.pads[0]["outAssign"] = korgPercOut
-	kitDef.pads[1]["outAssign"] = korgPercOut
-	kitDef.pads[2]["outAssign"] = korgPercOut
-	kitDef.pads[3]["outAssign"] = topKitOut
-	kitDef.pads[4]["outAssign"] = korgPercOut
-	kitDef.pads[5]["outAssign"] = korgPercOut
-	kitDef.pads[6]["outAssign"] = midKitOut
-	kitDef.pads[7]["outAssign"] = korgPercOut
-	kitDef.pads[8]["outAssign"] = korgPercOut
-	kitDef.pads[9]["outAssign"] = midKitOut
-	kitDef.pads[10]["outAssign"] = midKitOut
-	kitDef.pads[11]["outAssign"] = midKitOut
-	kitDef.pads[12]["outAssign"] = topKitOut
+    topKitOut = padOutFx1 if (c.fx1On() == 1) else padOutFx2
+    korgPercOut = padOutMaster if (c.inAssign == sub) else topKitOut
+    midKitOut = padOutFx2
+    bdOut = padOutSub # allow padOutFx2 if not RingMod
 
-	Kit().buildNamed(kitDef, "E:\\Roland\\SPD-SX\\KIT", idx)
-	idx += 1
+    kitDef.pads[0]["outAssign"] = korgPercOut
+    kitDef.pads[1]["outAssign"] = korgPercOut
+    kitDef.pads[2]["outAssign"] = korgPercOut
+    kitDef.pads[3]["outAssign"] = topKitOut
+    kitDef.pads[4]["outAssign"] = korgPercOut
+    kitDef.pads[5]["outAssign"] = korgPercOut
+    kitDef.pads[6]["outAssign"] = midKitOut
+    kitDef.pads[7]["outAssign"] = korgPercOut
+    kitDef.pads[8]["outAssign"] = korgPercOut
+    kitDef.pads[9]["outAssign"] = midKitOut
+    kitDef.pads[10]["outAssign"] = midKitOut
+    kitDef.pads[11]["outAssign"] = midKitOut
+    kitDef.pads[12]["outAssign"] = topKitOut
+
+    Kit().buildNamed(kitDef, os.path.join(loc, "KIT"), idx)
+    idx += 1
 
--- py/kit.py
+++ py/kit.py
@@ -3,87 +3,88 @@
 from datetime import date
 import xml.dom.minidom
 from effects import Thru
+import os
 
 def kitNode(doc, onto, name):
-	return onto.appendChild(doc.createElement(name))
+    return onto.appendChild(doc.createElement(name))
 
 def kitParam(doc, onto, name, value):
-	kitNode(doc, onto, name).appendChild(doc.createTextNode(str(value)))
-	
+    kitNode(doc, onto, name).appendChild(doc.createTextNode(str(value)))
+    
 def setName(doc, onto, name):
-	for i in range(8):
-		kitParam(doc, onto, "Nm%d" % i, ord(name[i]))
+    for i in range(8):
+    	kitParam(doc, onto, "Nm%d" % i, ord(name[i]))
 
 def valueFrom(padSpecs, item, i, alternative = -1):
-	if (i < len(padSpecs)):
-		spec = padSpecs[i]
-		if (item in spec):
-			return spec[item]
-	return alternative
-	
+    if (i < len(padSpecs)):
+    	spec = padSpecs[i]
+    	if (item in spec):
+    		return spec[item]
+    return alternative
+    
 def setUpFx(doc, onto, fx, n):
-	kitParam(doc, onto, "Fx%dSw" % n, 1 if fx.type > 0 else 0)
-	kitParam(doc, onto, "Fx%dType" % n, fx.type)
-	for i in range(20):
-		kitParam(doc, onto, "Fx%dPrm%d" % (n, i), fx.asSpec()[i] if i < len(fx.asSpec()) else 0)
+    kitParam(doc, onto, "Fx%dSw" % n, 1 if fx.type > 0 else 0)
+    kitParam(doc, onto, "Fx%dType" % n, fx.type)
+    for i in range(20):
+    	kitParam(doc, onto, "Fx%dPrm%d" % (n, i), fx.asSpec()[i] if i < len(fx.asSpec()) else 0)
 
-		
+    	
 class Kit():
-	def buildFrom(self, kitDef, name):
-		doc = xml.dom.minidom.parseString("<KitPrm/>")
-		kitPrm = doc.documentElement
+    def buildFrom(self, kitDef, name):
+    	doc = xml.dom.minidom.parseString("<KitPrm/>")
+    	kitPrm = doc.documentElement
 
-		kitParam(doc, kitPrm, "Level", kitDef.level)
-		kitParam(doc, kitPrm, "Tempo", kitDef.tempo * 10)
-		setName(doc, kitPrm, name)
-		
-		for i in range(16):
-			kitParam(doc, kitPrm, "SubNm%d" % i, 0)
+    	kitParam(doc, kitPrm, "Level", kitDef.level)
+    	kitParam(doc, kitPrm, "Tempo", kitDef.tempo * 10)
+    	setName(doc, kitPrm, name)
+    	
+    	for i in range(16):
+    		kitParam(doc, kitPrm, "SubNm%d" % i, 0)
 
-		kitParam(doc, kitPrm, "Fx2Asgn", 1 if hasattr(kitDef, "fx2") else 0) # does what?
-		kitParam(doc, kitPrm, "LinkPad0", -1)
-		kitParam(doc, kitPrm, "LinkPad1", -1)
-		setUpFx(doc, kitPrm, kitDef.fx1 if hasattr(kitDef, "fx1") else Thru(), 1)
-		setUpFx(doc, kitPrm, kitDef.fx2 if hasattr(kitDef, "fx2") else Thru(), 2)
-		
-		pan = kitDef.pan if hasattr(kitDef, "pan") else 15
-		
-		for i in range(15):
-			isPad = i < 13
-			
-			outAssign = 1 if (isPad and hasattr(kitDef, "fx1")) else 0
-			outAssign = valueFrom(kitDef.pads, "outAssign", i, outAssign) 
+    	kitParam(doc, kitPrm, "Fx2Asgn", 1 if hasattr(kitDef, "fx2") else 0) # does what?
+    	kitParam(doc, kitPrm, "LinkPad0", -1)
+    	kitParam(doc, kitPrm, "LinkPad1", -1)
+    	setUpFx(doc, kitPrm, kitDef.fx1 if hasattr(kitDef, "fx1") else Thru(), 1)
+    	setUpFx(doc, kitPrm, kitDef.fx2 if hasattr(kitDef, "fx2") else Thru(), 2)
+    	
+    	pan = kitDef.pan if hasattr(kitDef, "pan") else 15
+    	
+    	for i in range(15):
+    		isPad = i < 13
+    		
+    		outAssign = 1 if (isPad and hasattr(kitDef, "fx1")) else 0
+    		outAssign = valueFrom(kitDef.pads, "outAssign", i, outAssign) 
 
-			pad = kitNode(doc, kitPrm, "PadPrm")
-			kitParam(doc, pad, "Wv", valueFrom(kitDef.pads, "sound", i))
-			kitParam(doc, pad, "WvLevel", valueFrom(kitDef.pads, "vol", i, 100))
-			kitParam(doc, pad, "WvPan", pan)
-			kitParam(doc, pad, "PlayMode", 0)
-			kitParam(doc, pad, "OutAsgn", outAssign) 
-			kitParam(doc, pad, "MuteGrp", 0)
-			kitParam(doc, pad, "TempoSync", 0)
-			kitParam(doc, pad, "PadMidiCh", valueFrom(kitDef.pads, "channel", i))
-			kitParam(doc, pad, "NoteNum", valueFrom(kitDef.pads, "note", i))
-			kitParam(doc, pad, "MidiCtrl", 0)
-			kitParam(doc, pad, "Loop", 0)
-			kitParam(doc, pad, "TrigType", 0)
-			kitParam(doc, pad, "GateTime", -1)
-			kitParam(doc, pad, "Dynamics", 1)
-			kitParam(doc, pad, "VoiceAsgn", 1)
-			kitParam(doc, pad, "Reverse", 0)
-			kitParam(doc, pad, "SubWv", valueFrom(kitDef.pads, "soundb", i))
-			kitParam(doc, pad, "SubWvLevel", 80)
-			kitParam(doc, pad, "SubWvPan", 30)
-			
-		return kitPrm
-		
-	def build(self, kitDef, name, dir, zIndex):
-		kitFn = "kit%03d.spd" % zIndex
-		file = open("%s\\%s" % (dir, kitFn), "w")
-		self.buildFrom(kitDef, name).writexml(file, addindent="\t", newl="\n")
-		file.close()
-		print("kit %.3d %s created in %s" % (zIndex + 1, name, kitFn))
-	
-	def buildNamed(self, kitDef, dir, zIndex):
-		self.build(kitDef, kitDef.__name__, dir, zIndex)
+    		pad = kitNode(doc, kitPrm, "PadPrm")
+    		kitParam(doc, pad, "Wv", valueFrom(kitDef.pads, "sound", i))
+    		kitParam(doc, pad, "WvLevel", valueFrom(kitDef.pads, "vol", i, 100))
+    		kitParam(doc, pad, "WvPan", pan)
+    		kitParam(doc, pad, "PlayMode", 0)
+    		kitParam(doc, pad, "OutAsgn", outAssign) 
+    		kitParam(doc, pad, "MuteGrp", 0)
+    		kitParam(doc, pad, "TempoSync", 0)
+    		kitParam(doc, pad, "PadMidiCh", valueFrom(kitDef.pads, "channel", i))
+    		kitParam(doc, pad, "NoteNum", valueFrom(kitDef.pads, "note", i))
+    		kitParam(doc, pad, "MidiCtrl", 0)
+    		kitParam(doc, pad, "Loop", 0)
+    		kitParam(doc, pad, "TrigType", 0)
+    		kitParam(doc, pad, "GateTime", -1)
+    		kitParam(doc, pad, "Dynamics", 1)
+    		kitParam(doc, pad, "VoiceAsgn", 1)
+    		kitParam(doc, pad, "Reverse", 0)
+    		kitParam(doc, pad, "SubWv", valueFrom(kitDef.pads, "soundb", i))
+    		kitParam(doc, pad, "SubWvLevel", 80)
+    		kitParam(doc, pad, "SubWvPan", 30)
+    		
+    	return kitPrm
+    	
+    def build(self, kitDef, name, kitDir, zIndex):
+    	kitFn = "kit%03d.spd" % zIndex
+    	file = open(os.path.join(kitDir, kitFn), "w")
+    	self.buildFrom(kitDef, name).writexml(file, addindent="\t", newl="\n")
+    	file.close()
+    	print("kit %.3d %s created in %s" % (zIndex + 1, name, kitFn))
+    
+    def buildNamed(self, kitDef, dir, zIndex):
+    	self.build(kitDef, kitDef.__name__, dir, zIndex)
 
