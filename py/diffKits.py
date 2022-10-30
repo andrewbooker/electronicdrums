590272d88d65eb990e2c850e064c278524b378d9
Andrew Booker <improvizone@gmail.com>
2022-10-30 20:54:37 +0000

    getting kit differ working inc whitespace etc

--- py/diffKits.py
+++ py/diffKits.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python
 
 import os
-import glob
+import sys
 
-locDump = "D:\\gear\\spd-sx\\kit dumps\\2019-05-27"
-locGen = "D:\\gear\\spd-sx\\sandbox\\Roland\\SPD-SX\\KIT"
+if len(sys.argv) < 3:
+    print("usage:")
+    print("./diffKits.py <original-location> <test-location>")
+    exit()
 
-#actuals = glob.glob("%s\\*" % locDump)
-actuals = os.listdir(locDump)
+kitLoc = os.path.join("Roland", "SPD-SX", "KIT")
+locLive = os.path.join(sys.argv[1], kitLoc)
+locGen = os.path.join(sys.argv[2], kitLoc)
+
+actuals = os.listdir(locLive)
+generated = os.listdir(locGen)
 ignore = []#["kit057.spd", "kit061.spd", "kit062.spd", "kit068.spd"]
 f = 0
-for a in actuals:
-	actual = open("%s\\%s" % (locDump, a), "r")
-	gfn = "%s\\%s" % (locGen, a)
-	if (os.path.isfile(gfn) and int(a[3:6]) > 49 and a not in ignore):
-		anything = False
-		generated = open("%s\\%s" % (locGen, a), "r")
-		
-		
-		
-		als = actual.readlines()
-		gls = generated.readlines()
-		i = 0
-		
-		for al in als:
-			if ((len(gls) > i) and (al != gls[i])):
-				if (not anything):
-					print(a)
-					anything = True
-				print("%d: actual %s != generated %s" % (i, al, gls[i]))
-				f += 1
-			i += 1
-		
-		generated.close()
-	actual.close()
-	
-print("%d diff%s found" % (f, "s" if (f != 1) else ""))
-		
-	
+for g in generated:
+    gfn = os.path.join(locGen, g)
 
+    if (os.path.isfile(gfn) and g not in ignore):
+        generated = open(gfn, "r")
+        actual = open(os.path.join(locLive, g), "r")
+        anything = False
+
+        als = actual.readlines()
+        gls = generated.readlines()
+        i = 0
+
+        for al in als:
+            if ((len(gls) > i) and (al != gls[i])):
+                if (not anything):
+                    print(g)
+                    anything = True
+                print("%d: actual %s != generated %s" % (i, al, gls[i]))
+                f += 1
+            i += 1
+
+        generated.close()
+    actual.close()
+
+print("%d diff%s found" % (f, "s" if (f != 1) else ""))
