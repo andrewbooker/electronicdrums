731f8dbee9581c0c0da206a5f03738fcf8aaec5e
Andrew Booker <improvizone@gmail.com>
2022-11-05 13:49:35 +0000

    reading basic pad settings from spdsx xml spec

--- py/kitToJson.py
+++ py/kitToJson.py
@@ -4,6 +4,24 @@ from xml.dom import minidom
 import sys
 import json
 from effects import *
+from sounds import sounds
+
+padOrder = [
+    "topLeft",
+    "topCentre",
+    "topRight",
+    "midLeft",
+    "midCentre",
+    "midRight",
+    "bottomLeft",
+    "bottomCentre",
+    "bottomRight",
+    "footRight",
+    "footLeft",
+    "padTop",
+    "padRim"
+]
+
 
 doc = minidom.parse(sys.argv[1])
 
@@ -25,18 +43,36 @@ def effectFrom(node, n):
         return Reverb.fromSpec(a)
     return {}
 
+def soundFrom(i):
+    for s in sounds:
+        if sounds[s] == i:
+            return s
+    return ""
+
 kit = {}
 
 kit["name"] = kitNameFrom(doc)
 
 root = doc.getElementsByTagName("KitPrm")[0]
-
 for p in root.childNodes:
     if p.nodeType != p.TEXT_NODE:
         if p.tagName == "Level":
             kit["level"] = int(p.childNodes[0].data)
         if p.tagName == "Tempo":
             kit["tempo"] = int(0.1 * int(p.childNodes[0].data))
+
+
+pads = doc.getElementsByTagName("PadPrm")
+
+kit["pads"] = []
+
+for p in range(len(pads[:13])):
+    pad = {}
+    for c in pads[p].childNodes:
+        if c.nodeType != c.TEXT_NODE:
+            if c.tagName == "Wv":
+                pad["wav"] = soundFrom(int(c.childNodes[0].data))
+    kit["pads"].append({ padOrder[p]: pad })
 
 kit["fx1"] = effectFrom(doc, 1)
 
