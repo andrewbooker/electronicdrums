470c9b333d3fa028601c73ade851fa3fb8ccb411
Andrew Booker <improvizone@gmail.com>
2022-11-13 10:27:41 +0000

    pulling out method to convert single kit

--- py/kits/fromJson.py
+++ py/kits/fromJson.py
@@ -62,37 +62,42 @@ class FromJson():
             if "masterFx" in sysSetup:
                 sysConf.masterFx = getattr(effects, sysSetup["masterFx"]["type"])()
 
+    @staticmethod
+    def asKit(kit):
+        k = type(kit["name"], (), {})
+        k.level = kit["level"] if "level" in kit else 100
+        k.tempo = kit["tempo"]
+        k.pads = []
+        if "korg" in kit:
+            k.korg = kit["korg"]
+
+        for i in range(13):
+            pad = {}
+            if padNames[i] in kit["pads"]:
+                padSpec = kit["pads"][padNames[i]]
+                if "wav" in padSpec:
+                    pad["sound"] = wav[padSpec["wav"]]
+                if "outAssign" in padSpec:
+                    pad["outAssign"] = padOutAssign[padSpec["outAssign"]]
+                if "vol" in padSpec:
+                    pad["vol"] = padSpec["vol"]
+                if "note" in padSpec:
+                    pad["note"] = padSpec["note"]
+                if "channel" in padSpec:
+                    pad["channel"] = padSpec["channel"]
+            k.pads.append(pad)
+
+        if "fx1" in kit:
+            eff = kit["fx1"]
+            k.fx1 = getattr(effects, eff["type"])()
+            for p in eff["params"]:
+                getattr(k.fx1, p)(int(eff["params"][p]))
+
+        return k
+
     def kits(self):
         kits = []
 
         for kit in self.bandSet["kits"]:
-            k = type(kit["name"], (), {})
-            k.level = kit["level"] if "level" in kit else 100
-            k.tempo = kit["tempo"]
-            k.pads = []
-            if "korg" in kit:
-                k.korg = kit["korg"]
-
-            for i in range(13):
-                pad = {}
-                if padNames[i] in kit["pads"]:
-                    padSpec = kit["pads"][padNames[i]]
-                    if "wav" in padSpec:
-                        pad["sound"] = wav[padSpec["wav"]]
-                    if "outAssign" in padSpec:
-                        pad["outAssign"] = padOutAssign[padSpec["outAssign"]]
-                    if "vol" in padSpec:
-                        pad["vol"] = padSpec["vol"]
-                    if "note" in padSpec:
-                        pad["note"] = padSpec["note"]
-                    if "channel" in padSpec:
-                        pad["channel"] = padSpec["channel"]
-                k.pads.append(pad)
-
-            if "fx1" in kit:
-                eff = kit["fx1"]
-                k.fx1 = getattr(effects, eff["type"])()
-                for p in eff["params"]:
-                    getattr(k.fx1, p)(int(eff["params"][p]))
-            kits.append(k)
+            kits.append(FromJson.asKit(kit))
         return kits
