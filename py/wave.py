bb1defecdd0c69073ca77de845fa344bb58b92c8
Andrew Booker <improvizone@gmail.com>
2026-09-01 00:39:46 +0100

    naming prm file based on condensed sample file numbering

--- py/wave.py
+++ py/wave.py
@@ -9,25 +9,6 @@ def param(doc, onto, name, value):
     onto.appendChild(doc.createElement(name)).appendChild(doc.createTextNode(str(value)))
 
 
-def prm(fqfnOut, name, wavPath):
-    doc = xml.dom.minidom.parseString("<WvPrm/>")
-    wvPrm = doc.documentElement
-
-    for i in range(12):
-        param(doc, wvPrm, "Nm%d" % i, ord(name[i]))
-
-    param(doc, wvPrm, "Tag", 0)
-    param(doc, wvPrm, "Tempo", 1200)
-    param(doc, wvPrm, "Beat", 0)
-    param(doc, wvPrm, "Measure", 0)
-    param(doc, wvPrm, "Start", 0)
-    param(doc, wvPrm, "End", 0)
-    param(doc, wvPrm, "Path", wavPath)
-
-    file = open(fqfnOut, "w")
-    wvPrm.writexml(file, addindent="\t", newl="\n")
-    file.close()
-
 
 class Wave():
     def __init__(self, baseDir, idx, fn):
@@ -48,12 +29,33 @@ class Wave():
             os.remove(fqfn)
         self.file = sf.SoundFile(fqfn, mode="x", samplerate=44100, channels=1, subtype="PCM_16")
 
+    def _prm(self):
+        fqfnOut = os.path.join(self.prmDir, self.subDir, f"{self.fn[5:7]}.spd")
+        print("creating", fqfnOut)
+        baseName = self.fn.split(".")[0]
+        name = f"gen_{baseName}"
+        doc = xml.dom.minidom.parseString("<WvPrm/>")
+        wvPrm = doc.documentElement
+
+        for i in range(12):
+            param(doc, wvPrm, "Nm%d" % i, ord(name[i]))
+
+        param(doc, wvPrm, "Tag", 0)
+        param(doc, wvPrm, "Tempo", 1200)
+        param(doc, wvPrm, "Beat", 0)
+        param(doc, wvPrm, "Measure", 0)
+        param(doc, wvPrm, "Start", 0)
+        param(doc, wvPrm, "End", 0)
+        param(doc, wvPrm, "Path", self.fn)
+
+        file = open(fqfnOut, "w")
+        wvPrm.writexml(file, addindent="\t", newl="\n")
+        file.close()
+
     def write(self, d):
         self.file.write(d)
 
     def close(self):
         self.file.close()
-        fqn = os.path.join(self.prmDir, self.subDir, f"{self.idx:02d}.spd")
-        print("creating", fqn)
-        prm(fqn, "iz_%.10d" % self.idx, self.fn)
+        self._prm()
 
