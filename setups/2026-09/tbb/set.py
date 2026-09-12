1a811c873f5beb972da10f6862d69af9dca5ed76
Andrew Booker <improvizone@gmail.com>
2026-09-12 18:13:57 +0100

    including SPD-SX program changes

--- setups/2026-09/tbb/set.py
+++ setups/2026-09/tbb/set.py
@@ -9,10 +9,15 @@ portNames = mido.get_output_names()
 portName = portNames[1]
 
 outport = mido.open_output(portName)
-korgChannel = 0
+
+channels = {
+    "korg": 0,
+    "spd-sx": 13
+}
+
 
 setList = [
-    {"song": "The Warm-Up Man Forever", "tempo": 122, "korg": 19, "spd-sx": 27},
+    {"song": "The Warm-Up Man Forever", "tempo": 122, "korg": 19, "spd-sx": 26},
     {"song": "Sing To Me", "tempo": 63},
     {"song": "Watching Over Me", "tempo": 102},
     {"song": "Sorry Looking Soldier", "tempo": 42},
@@ -20,17 +25,22 @@ setList = [
     {"song": "A Stand-Up For The Dying", "tempo": 60},
     {"song": "Wherever There Is Light", "tempo": 60},
     {"song": "Rainmark", "tempo": 126},
-    {"song": "Days Turn Into Years", "tempo": 84, "korg": 4, "spd-sx": 20},
+    {"song": "Days Turn Into Years", "tempo": 84, "korg": 4, "spd-sx": 19},
     {"song": "Pretty Genius", "tempo": 69},
-    {"song": "Time Travel In Texas", "tempo": 78, "spd-sx": 26},
-    {"song": "Mixtaped", "tempo": 47, "korg": 21, "spd-sx": 25}
+    {"song": "Time Travel In Texas", "tempo": 78, "spd-sx": 25},
+    {"song": "Mixtaped", "tempo": 47, "korg": 21, "spd-sx": 24}
 ]
 
 
+def sendProgramChange(song, instr):
+    if instr in song:
+        outport.send(mido.Message("program_change", channel=channels[instr], program=song[instr]))
+
 def selectSong(idx):
     song = setList[idx]
-    if "korg" in song:
-        outport.send(mido.Message("program_change", channel=korgChannel, program=int(song["korg"])))
+    sendProgramChange(song, "korg")
+    sendProgramChange(song, "spd-sx")
+
 
 for i in range(len(setList)):
     s = setList[i]
@@ -41,9 +51,6 @@ songIdx = 0
 selectSong(songIdx)
 
 
-outport.send(mido.Message("note_on", channel=korgChannel, note=60, velocity=64, time=0))
+outport.send(mido.Message("note_on", channel=channels["korg"], note=60, velocity=64, time=0))
 time.sleep(1)
-outport.send(mido.Message("note_off", channel=korgChannel, note=60, velocity=0, time=0))
-
-
-
+outport.send(mido.Message("note_off", channel=channels["korg"], note=60, velocity=0, time=0))
