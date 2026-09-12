e855ef7faa15a74f8340be768885ac9fba9b109e
Andrew Booker <improvizone@gmail.com>
2026-09-12 14:17:43 +0100

    set list with tempos for TBB 2026

---
+++ setups/2026-09/tbb/set.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+setList = [
+("The Warm-Up Man Forever", 120),
+("Sing To Me", 63),
+("Watching Over Me", 102),
+("Sorry Looking Soldier", 42),
+("Brightest Blue", 84),
+("A Stand-Up For The Dying", 60),
+("Wherever There Is Light", 60),
+("Rainmark", 126),
+("Days Turn Into Years", 84),
+("Pretty Genius", 69),
+("Time Travel In Texas", 78),
+("Mixtaped", 47)
+]
+
+print(setList)
