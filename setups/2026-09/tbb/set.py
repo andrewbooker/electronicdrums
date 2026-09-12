9f0688834ba108eddf7b7d1303d4dc0a06c672f8
Andrew Booker <improvizone@gmail.com>
2026-09-12 17:36:31 +0100

    more organised set list and demoing sending midi messages

--- setups/2026-09/tbb/set.py
+++ setups/2026-09/tbb/set.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
 
+# sudo pip install mido
+# sudo pip install python-rtmidi
+import mido
+import time
+
+
 setList = [
-("The Warm-Up Man Forever", 120),
-("Sing To Me", 63),
-("Watching Over Me", 102),
-("Sorry Looking Soldier", 42),
-("Brightest Blue", 84),
-("A Stand-Up For The Dying", 60),
-("Wherever There Is Light", 60),
-("Rainmark", 126),
-("Days Turn Into Years", 84),
-("Pretty Genius", 69),
-("Time Travel In Texas", 78),
-("Mixtaped", 47)
+    {"song": "The Warm-Up Man Forever", "tempo": 122, "korg": 19, "spd-sx": 27},
+    {"song": "Sing To Me", "tempo": 63},
+    {"song": "Watching Over Me", "tempo": 102},
+    {"song": "Sorry Looking Soldier", "tempo": 42},
+    {"song": "Brightest Blue", "tempo": 84},
+    {"song": "A Stand-Up For The Dying", "tempo": 60},
+    {"song": "Wherever There Is Light", "tempo": 60},
+    {"song": "Rainmark", "tempo": 126},
+    {"song": "Days Turn Into Years", "tempo": 84, "korg": 4, "spd-sx": 20},
+    {"song": "Pretty Genius", "tempo": 69},
+    {"song": "Time Travel In Texas", "tempo": 78, "spd-sx": 26},
+    {"song": "Mixtaped", "tempo": 47, "korg": 21, "spd-sx": 25}
 ]
 
-print(setList)
+
+
+for i in range(len(setList)):
+    s = setList[i]
+    print(f"{(1 + i):2d}", s["song"])
+
+
+print(mido.get_output_names())
+
+korgChannel = 0
+outport = mido.open_output("USB Midi:USB Midi MIDI 1 20:0")
+outport.send(mido.Message("note_on", channel=korgChannel, note=60, velocity=64, time=0))
+time.sleep(1)
+outport.send(mido.Message("note_off", channel=korgChannel, note=60, velocity=0, time=0))
