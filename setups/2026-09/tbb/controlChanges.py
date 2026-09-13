ae57c81d1a7ed0bf17c7a850ef3f0cdc7c67eab9
Andrew Booker <improvizone@gmail.com>
2026-09-13 13:46:32 +0100

    splitting out control changes from program specification

--- setups/2026-09/tbb/controlChanges.py
+++ setups/2026-09/tbb/controlChanges.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python3
+
 import sys
 import time
 import threading
 import random
 import readchar
 import math
+import mido
+
+
+portNames = mido.get_output_names()
+portName = portNames[1]
+outport = mido.open_output(portName)
 
 
 width = 20
@@ -31,21 +39,36 @@ class HorizBar:
         self.matrix[idx][p] = "|"
 
 
-class Writer:
-    def __init__(self, writeTo):
-        self.writeTo = writeTo
+class ControlChangeSender:
+    def __init__(self, channel, number, midiOut, freq, renderer, idx):
+        self.channel = channel
+        self.number = number
+        self.midiOut = midiOut
+        self.freq = freq
+        self.renderer = renderer
+        self.idx = idx
 
     def start(self, shouldStop):
+        start_time = time.monotonic()
+        interval = 0.2
         while not shouldStop.is_set():
-            for i in range(4):
-                self.writeTo.write(i, random.random());
-                time.sleep(random.random())
+            elapsed = time.monotonic() - start_time
+            v = math.sin(2 * math.pi * self.freq * elapsed)
+            outport.send(mido.Message("control_change", channel=self.channel, control=self.number, value=math.floor(56 * (1.0 + v))))
+            self.renderer.write(self.idx, v)
+            next_time = start_time + (int(elapsed / interval) + 1) * interval
+            time.sleep(max(0, next_time - time.monotonic()))
+
 
 hb = HorizBar()
 procs = [
-    hb,
-    Writer(hb)
+    ControlChangeSender(0, 12, outport, 0.2, hb, 0),
+    ControlChangeSender(0, 13, outport, 1.8, hb, 1),
+    ControlChangeSender(13, 12, outport, 0.5, hb, 2),
+    ControlChangeSender(13, 13, outport, 2.7, hb, 3),
+    hb
 ]
+
 shouldStop = threading.Event()
 threads = [threading.Thread(target=p.start, args=(shouldStop,), daemon=True) for p in procs]
 [t.start() for t in threads]
--- setups/2026-09/tbb/set.py
+++ setups/2026-09/tbb/set.py
@@ -11,7 +11,6 @@ import readchar
 
 portNames = mido.get_output_names()
 portName = portNames[1]
-
 outport = mido.open_output(portName)
 
 devices = {
@@ -21,38 +20,19 @@ devices = {
 
 
 setList = [
-    {"song": "The Warm-Up Man Forever", "tempo": 122, "korg": 19, "spd-sx": 26},
-    {"song": "Sing To Me", "tempo": 63},
-    {"song": "Watching Over Me", "tempo": 102},
-    {"song": "Sorry Looking Soldier", "tempo": 42},
-    {"song": "Brightest Blue", "tempo": 84},
-    {"song": "A Stand-Up For The Dying", "tempo": 60},
-    {"song": "Wherever There Is Light", "tempo": 60},
-    {"song": "Rainmark", "tempo": 126},
-    {"song": "Days Turn Into Years", "tempo": 84, "korg": 4, "spd-sx": 19},
-    {"song": "Pretty Genius", "tempo": 69},
-    {"song": "Time Travel In Texas", "tempo": 78, "spd-sx": 25},
-    {"song": "Mixtaped", "tempo": 47, "korg": 21, "spd-sx": 24}
+    {"name": "Warm-Up Man Forever", "tempo": 122, "korg": 19, "spd-sx": 26},
+    {"name": "Sing To Me", "tempo": 63},
+    {"name": "Watching Over Me", "tempo": 102},
+    {"name": "Sorry Looking Soldier", "tempo": 42},
+    {"name": "Brightest Blue", "tempo": 84},
+    {"name": "Stand-Up For The Dying", "tempo": 60},
+    {"name": "Wherever There Is Light", "tempo": 60},
+    {"name": "Rainmark", "tempo": 126},
+    {"name": "Days Turn Into Years", "tempo": 84, "korg": 4, "spd-sx": 19},
+    {"name": "Pretty Genius", "tempo": 69},
+    {"name": "Time Travel In Texas", "tempo": 78, "spd-sx": 25},
+    {"name": "Mixtaped", "tempo": 47, "korg": 21, "spd-sx": 24}
 ]
-
-
-class ControlChangeSender:
-    def __init__(self, channel, number, midiOut, freq):
-        self.channel = channel
-        self.number = number
-        self.midiOut = midiOut
-        self.freq = freq
-
-    def start(self, shouldStop):
-        start_time = time.monotonic()
-        interval = 0.2
-        while not shouldStop.is_set():
-            elapsed = time.monotonic() - start_time
-            v = math.sin(2 * math.pi * self.freq * elapsed)
-            outport.send(mido.Message("control_change", channel=self.channel, control=self.number, value=math.floor(56 * (1.0 + v))))
-
-            next_time = start_time + (int(elapsed / interval) + 1) * interval
-            time.sleep(max(0, next_time - time.monotonic()))
 
 
 def sendProgramChange(song, instr):
@@ -60,42 +40,19 @@ def sendProgramChange(song, instr):
     c = devices[instr]["channel"]
     outport.send(mido.Message("program_change", channel=c, program=p))
 
+
 def selectSong(idx):
     song = setList[idx]
+    print("Setting to", song["name"])
     sendProgramChange(song, "korg")
     sendProgramChange(song, "spd-sx")
 
 
 for i in range(len(setList)):
-    s = setList[i]
-    print(f"{(1 + i):2d}", s["song"])
+    song = setList[i]
+    print(f"{(1 + i):2d}", song["name"])
 
 
 songIdx = 0
 selectSong(songIdx)
-
-
-controlSignals = [
-    ControlChangeSender(devices["korg"]["channel"], 12, outport, 0.2),
-    ControlChangeSender(devices["korg"]["channel"], 13, outport, 1.8),
-    ControlChangeSender(devices["spd-sx"]["channel"], 12, outport, 0.5),
-    ControlChangeSender(devices["spd-sx"]["channel"], 13, outport, 2.7)
-]
-
-shouldStop = threading.Event()
-threads = [threading.Thread(target=c.start, args=(shouldStop,), daemon=True) for c in controlSignals]
-[t.start() for t in threads]
-done = False
-
-print("Started. Press 'q' to exit")
-while not done:
-    c = readchar.readchar()
-    if c == "q":
-        done = True
-        shouldStop.set()
-        [t.join() for t in threads]
-    if c == "n":
-        outport.send(mido.Message("note_on", channel=0, note=60, velocity=64, time=0))
-        time.sleep(1)
-        outport.send(mido.Message("note_off", channel=0, note=60, velocity=0, time=0))
 
